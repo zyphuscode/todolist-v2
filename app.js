62c20c63cd01d1c7df05ed4462143e6606e146fb
Zehra Nisar <zyphuscode@gmail.com>
2020-04-27 21:11:06 +1000

    add lodash in customised list route to capitalize the first letter.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _= require("lodash");
 
 
 const app = express();
@@ -92,7 +93,7 @@ app.get("/", function(req, res) {
 
 // new routes with express routes paramater
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName =_.capitalize(req.params.customListName);
 
 
   List.findOne({name: customListName}, function(err, foundList){
