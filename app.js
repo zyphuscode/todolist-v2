a133023e6bb0f5aa147a40194b34d703dbfde97f
Zehra Nisar <zyphuscode@gmail.com>
2020-04-29 02:04:28 +1000

    Add git ignore, procfile and update ports

--- app.js
+++ app.js
@@ -183,7 +183,13 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+//heroku port set up
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
 });
